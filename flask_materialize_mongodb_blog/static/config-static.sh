2c63ba23b85b894141338c8a3c82efa2aae7785c
keysona <keypython@gmail.com>
2016-06-07 16:34:52 +0800

    Change materialize _grid.scss

--- flask_materialize_mongodb_blog/static/config-static.sh
+++ flask_materialize_mongodb_blog/static/config-static.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 # materialize
-sass --style compressed lib/materialize-src/sass/materialize.scss static/css/materialize.min.css
+# cp lib/materialize/js/bin/materialize.min.js static/js/
+# cp -r lib/materialize/fonts/roboto static/fonts
+# sass --style compressed lib/materialize/sass/materialize.scss static/css/materialize.min.css
+
+# font-awesome 4.6.1
+# cp lib/font-awesome/css/font-awesome.min.css static/css/
+# cp -r lib/font-awesome/fonts static/
 
 # my css
 sass --style compressed lib/keysona/keysona.scss static/css/keysona.min.css
---
+++ flask_materialize_mongodb_blog/static/lib/materialize/change.md
@@ -0,0 +1,6 @@
+# version 0.97.5
+
+sass/components/_grid.scss
+  line 13
+    change "width 70%" to "width 80"
+
