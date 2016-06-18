f983be3f8b31de3dd3bfc69f817545e3ccb48ba1
keysona <keypython@gmail.com>
2016-06-19 01:06:37 +0800

    Fix a bug.

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ VIRTUALENV_DIR=$VIRTUALENVWRAPPER_HOOK_DIR
 FLASK_BLOG_ENV=flask-blog
 echo $VIRTUALENV_DIR/$FLASK_BLOG_ENV/bin/activate
 source $VIRTUALENV_DIR/$FLASK_BLOG_ENV/bin/activate
-nohup gunicorn -w 4 --access-logfile /var/log/access-flask-blog.log --error-logfile /var/log/error-flask-blog.log -b 127.0.0.1:8080 manage:app 1>/dev/null 2>&1 &
+nohup gunicorn -w 4 --access-logfile /var/log/access-flask-blog.log --error-logfile /var/log/error-flask-blog.log -b 127.0.0.1:5001 manage:app 1>/dev/null 2>&1 &
