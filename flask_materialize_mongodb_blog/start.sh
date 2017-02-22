d759d5f17dc6faa7eaf089bef40baf51ab283139
keysona <keypython@gmail.com>
2017-02-22 16:35:40 +0800

    Fix start.sh.

--- flask_materialize_mongodb_blog/start.sh
+++ flask_materialize_mongodb_blog/start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
-export WORKON_HOME=$HOME/.virtualenvs
-source /usr/local/bin/virtualenvwrapper.sh
-
-workon flask-blog
-
 nohup \
 gunicorn -w 4 -b 127.0.0.1:5001 \
 --error-logfile /var/log/error-flask-blog.log \
